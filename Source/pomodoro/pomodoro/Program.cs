96cadee5d728645d91455affd26583703954b850
Stefan Hoffmann <sthoffmann@onlinehome.de>
2017-12-16 08:54:18 +0100

    new console project

---
+++ Source/pomodoro/pomodoro/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace pomodoro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
